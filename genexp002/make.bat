30bc34166fee6c4acc28eac34e2d2ed6d9487439
Michal Ziulek <michalziulek@gmail.com>
2018-08-31 15:04:41 +0200

    Add files via upload

--- genexp002/make.bat
+++ genexp002/make.bat
@@ -10,14 +10,21 @@ set DEBUG=/Zi /Od /D_DEBUG /MTd
 if not defined CONFIG set CONFIG=%DEBUG%
 set CFLAGS=%CONFIG% /EHa- /GR- /Gy /Gw /W3 /nologo /I"..\external"
 set LFLAGS=/incremental:no /opt:ref /machine:x64
+set FXC=fxc.exe /Ges /O3 /WX /nologo /Qstrip_reflect /Qstrip_debug /Qstrip_priv
 
 set ERROR=0
 
+if exist data\shaders\*.cso del data\shaders\*.cso
 if exist %NAME%.exe del %NAME%.exe
+
+%FXC% /D VS_IMGUI /E VsImgui /Fo data\shaders\imgui-vs.cso /T vs_5_1 %NAME%.hlsl & if ERRORLEVEL 1 (set ERROR=1 & goto :end)
+%FXC% /D PS_IMGUI /E PsImgui /Fo data\shaders\imgui-ps.cso /T ps_5_1 %NAME%.hlsl & if ERRORLEVEL 1 (set ERROR=1 & goto :end)
+
 if not exist %NAME%-external.pch cl %CFLAGS% /c /Yc%NAME%-external.h %NAME%-external.cpp
 cl %CFLAGS% /Yu%NAME%-external.h %NAME%-main.cpp /link %LFLAGS% %NAME%-external.obj kernel32.lib user32.lib gdi32.lib /out:%NAME%.exe
 if ERRORLEVEL 1 set ERROR=1
 if exist %NAME%-main.obj del %NAME%-main.obj
 if "%1" == "run" if exist %NAME%.exe %NAME%.exe
 
+:end
 exit /b %ERROR%
